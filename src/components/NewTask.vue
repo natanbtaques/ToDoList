ca795767324cd1d7d389e0bbe11860d6faca490b
Natan Carvalho de Barros Taques <61803375+natanbtaques@users.noreply.github.com>
2022-10-26 10:21:51 -0400

    Primeiro Commit

---
+++ src/components/NewTask.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="new-task">
+        <input class="form-element" v-model ="name" @keydown.enter="add" type="text" placeholder="New Task?">
+        <button class="form-element" @click="add" >+</button>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            name:''
+        }
+    },
+    methods:{
+        add(){
+            this.$emit('TaskAdded' , {name: this.name})
+            this.name= ''
+        }
+    }
+}
+</script>
+
+<style>
+    .new-task{
+        position:relative;
+        display: flex;
+        text-align: center;
+        align-items: center;
+        margin: 45px;
+        
+    }
+
+    .form-element{
+        font-size: 2rem;
+        outline: none;
+        border: 1px solid #FFF;
+        padding: 5px 10px 8px ;
+        color: #fff;
+        
+        
+        
+    }
+    input.form-element{
+        width: 500px;
+        background: #fff2;
+        border-top-left-radius: 8px;
+        border-bottom-left-radius: 8px;
+    }
+
+    button.form-element{
+        border-left: none;
+        color: #FFF;
+        background-color: rgba(103, 103, 255, 0.445);
+        border-top-right-radius: 8px;
+        border-bottom-right-radius: 8px;
+        
+    }
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,74 @@
+<template>
+    <div @click="$emit('taskComplete',task)"
+        class="task" :class="stateClass">
+        <span @click.stop="$emit('taskDeleted',task)" class="close">x</span>
+        <p> {{task.name }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    //define que o tipo de informação recebida será de um objeto, no caso desta aplicação
+    //os objetos serão os itens da lista, que possui o name e o pending
+    props:{
+        task: {type: Object, Required: true}
+    },
+    computed:{
+        stateClass(){
+            return{
+              pending: this.task.pending, 
+              done: !this.task.pending 
+            }
+        }
+    }
+}
+</script>
+
+<style>
+    .task{
+        position:relative;
+        box-sizing: border-box;
+        width: 350px;
+        height:150px;
+        padding: 10px;
+        border-radius:8px ;
+        font-size: 2rem;
+        font-weight: 300;
+        cursor:pointer;
+        user-select: none;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 0.5em;
+        margin: 0.5rem;
+    }
+    .pending{
+        border-left: 12px solid #B73229;
+        background-color: #f44336;
+    }
+    .done{
+        color:#ddd;
+        border-left: 12px solid #0A8F08;
+        background-color: #4caf50;
+        text-decoration: line-through;
+    }
+    .close{
+        position:absolute;
+        right:10px;
+        top:10px;
+        font-size:0.9rem;
+        font-weight: 600;
+        height: 20px;
+        width: 20px;
+        border-radius: 10px;
+        text-align: center;
+
+
+    }
+    .pending .close{
+        background-color:#B73229 ;
+    }
+    .done .close{
+        background-color:#0A8F08 ;
+    }
+</style>
